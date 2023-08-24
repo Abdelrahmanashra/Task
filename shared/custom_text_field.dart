1a37d070548add34bd79f364179ef03e136df034
Abdelrahmanashra <142675333+Abdelrahmanashra@users.noreply.github.com>
2023-08-24 17:52:34 +0300

    Add files via upload

---
+++ shared/custom_text_field.dart
@@ -0,0 +1,44 @@
+
+import 'package:flutter/material.dart';
+
+import '../constnats/my_colors.dart';
+Widget customTextFiled({required bool obscureText ,required TextInputType textInputType,required String hintText,required Widget label,required TextEditingController textcontroller}){
+
+  return TextFormField(
+    style: const TextStyle(color: Colors.black,letterSpacing: 0.5,fontSize: 18),
+    validator: (val){
+      if(val!.length<1 && val!.isNotEmpty)
+      {
+        return "Input less than 4 character";
+      }
+      if(val!.isEmpty){
+        return "Input can not be empty";
+      }
+    },
+    controller: textcontroller,
+    keyboardType:textInputType,
+    autofocus: true,
+    obscureText: obscureText,
+    decoration: InputDecoration(
+      enabledBorder: OutlineInputBorder(
+        borderSide:
+        BorderSide(width: 3, color: Colors.grey), //<-- SEE HERE
+        borderRadius: BorderRadius.circular(50.0),
+      ),
+      label: label,
+      labelStyle: const TextStyle(fontSize: 18,color: Colors.black),
+      hintText: hintText,
+      hintStyle: const TextStyle(color: Colors.grey),
+
+      border: const UnderlineInputBorder(
+        borderSide: BorderSide(
+            width: 3, color: MyColors.darkBlue),
+      ),
+
+      // enabledBorder: const UnderlineInputBorder(
+      //   borderSide: BorderSide(
+      //       width: 3, color: Colors.grey),
+      // ),
+    ),
+  );
+}
